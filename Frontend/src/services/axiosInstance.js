7e5a8f802c91f6350453dcccec97ca06eb978e3b
Thejaswini2005 <thejaswinigangavaram@gmail.com>
2026-06-20 16:26:35 +0530

    Update API URL for production deployment

--- Frontend/src/services/axiosInstance.js
+++ Frontend/src/services/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL:
+    "https://smart-hostel-complaint-management-system.onrender.com",
 });
 
 axiosInstance.interceptors.request.use((config) => {
